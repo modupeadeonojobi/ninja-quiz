32126548b490ef03c09961257f4f5405a988c396
Modupe Ade-Onojobi <66411673+modupeadeonojobi@users.noreply.github.com>
2020-08-21 01:14:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const correctAnswers = ['B', 'A', 'B', 'A'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    //check answers
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]; 
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+            score += 25;
+        }
+    });
+    scrollTo(0,0);
+    result.classList.remove('d-none');
+
+    //animate the score
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if (output === score){
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+});
